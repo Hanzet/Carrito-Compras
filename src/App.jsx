d1681fe4e46c9efff3cc730a3535bde30adedd74
Jonathan Orrego Montoya <jonathan.orrego@ucp.edu.co>
2025-02-18 22:32:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { db } from "./data/db"
+
+function App() {
+
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+
+    function addToCart(item) {
+
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if (itemExists >= 0) { // Existe en el carrito
+            const updateCart = [...cart] // Copia del carrito
+            updateCart[itemExists].quantity ++ // Actualizamos la cantidad
+            setCart(updateCart) // Actualizamos el carrito
+        } else {
+            item.quantity = 1 // Agregamos la cantidad
+            setCart([...cart, item]) // Actualizamos el carrito
+        }
+    }
+
+    return (
+        <>
+
+            <Header 
+                cart={cart}
+            />
+
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    {data.map((guitar) => (
+                        // console.log(guitar),
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
+                            setCart={setCart}
+                            // cart={cart}
+                            addToCart={addToCart}
+                        />
+                    ))}
+                </div>
+            </main>
+
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados 2025 - Jonacode -</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default App
